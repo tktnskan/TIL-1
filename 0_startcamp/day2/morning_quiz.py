5a8160b9b880c201beacaa001e676469662899e3
eduyu <neo@hphk.kr>
2018-12-19 10:02:31 +0900

    showing default vi

--- 0_startcamp/day2/morning_quiz.py
+++ 0_startcamp/day2/morning_quiz.py
@@ -1,1 +1,12 @@
-print('good morning')
+# 1. 평균을 구하시오
+my_score = [79, 84, 66, 93]
+# my_average # float
+
+
+your_score = {
+    '수학': 87,
+    '국어': 83,
+    '영어': 76,
+    '도덕': 100
+}
+# your_average # float
