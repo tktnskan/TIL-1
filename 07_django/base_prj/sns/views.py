a407abe2bf0a5d9b861b8de496038ec27395397c
eduyu <neo@hphk.kr>
2019-02-11 14:13:42 +0900

    views and urls

--- 07_django/base_prj/sns/views.py
+++ 07_django/base_prj/sns/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Posting, Comment
 
-# Create your views here.
+def posting_list(request):
+    pass
+
+def posting_detail(request):
+    pass
+
+def create_posting(request):
+    pass
+
+def edit_posting(request):
+    pass
+
+def delete_posting(request):
+    pass
