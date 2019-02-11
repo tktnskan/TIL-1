ff5bc19d1d78b4b8d1e3614743eef682b5f1c938
eduyu <neo@hphk.kr>
2019-02-11 13:56:11 +0900

    base prj

---
+++ 07_django/base_prj/sns/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Posting, Comment
+
+class PostingModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Posting, PostingModelAdmin)
+
+class CommentModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Comment, CommentModelAdmin)
+
