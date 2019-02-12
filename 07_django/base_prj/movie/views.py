8d6833be5f2cdb36dd7736ee64088761dfa25c32
eduyu <neo@hphk.kr>
2019-02-12 11:28:18 +0900

    django form

--- 07_django/base_prj/movie/views.py
+++ 07_django/base_prj/movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Movie
+from .forms import MovieForm
 from IPython import embed
 
 # Create your views here.
@@ -11,23 +12,28 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        title_eng = request.POST.get('title_eng')
-        audience = int(request.POST.get('audience'))
-        open_date = request.POST.get('open_date')
-        genre = request.POST.get('genre')
-        watch_grade = request.POST.get('watch_grade')
-        score = float(request.POST.get('score'))
-        # embed()
-        poster_url = request.POST.get('poster_url')
-        description = request.POST.get('description')
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            title_eng = form.cleaned_data.get('title_eng')
+            audience = form.cleaned_data.get('audience')
+            open_date = form.cleaned_data.get('open_date')
+            genre = form.cleaned_data.get('genre')
+            watch_grade = form.cleaned_data.get('watch_grade')
+            score = form.cleaned_data.get('score')
+            # embed()
+            poster_url = form.cleaned_data.get('poster_url')
+            description = form.cleaned_data.get('description')
 
-        Movie.objects.create(
-            title=title, title_eng=title_eng, audience=audience,
-            open_date=open_date, genre=genre, watch_grade=watch_grade,
-            score=score, poster_url=poster_url, description=description,
-        )
-
-        return redirect('movie:list')
+            Movie.objects.create(
+                title=title, title_eng=title_eng, audience=audience,
+                open_date=open_date, genre=genre, watch_grade=watch_grade,
+                score=score, poster_url=poster_url, description=description,
+            )
+            return redirect('movie:list')
     else:
-        return render(request, 'movie/create.html')
+        form = MovieForm()
+
+    return render(request, 'movie/create.html', {
+        'form': form,
+    })
